895169be755095d2f5cbe84ee2ab65ff197ead4d
Ben <ben.godin@hotmail.ca>
2021-06-29 20:37:48 -0400

    added clap arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use nes::console::Console;
+use rustynes::console::Console;
 use std::path::Path;
-use nes::clock::Clocked;
+use rustynes::clock::Clocked;
 use std::fs::File;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    // TODO: Fix SHY, SHX
-    let path = Path::new("./loz.nes");
+    let yaml = load_yaml!("./config/clap_args.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let rom = matches.value_of("ROM").unwrap();
+    let path = Path::new(rom);
     let mut console = Console::new(path);
     console.cycle();
 }
